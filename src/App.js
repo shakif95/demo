1028a4b5183965b5a14873e93d1a602c33ac147c
Shakif95 <shakif.malek@gmail.com>
2020-11-18 12:02:57 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+export const App = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data.slice(0, 10)));
+  }, []);
+
+  const handleDeletePost = postId => {
+    setPosts(posts.filter(x => x.id !== postId));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='text-center'>{posts.length} posts found!</div>
+      <div className='w-50 m-auto'>
+        {
+          posts && posts.length > 0 &&
+          posts.map(post => (
+            <div className='card my-3' key={post.id}
+            >
+              <div className='card-body'>
+                <div className='text-right'>
+                  <button className='btn btn-sm btn-outline-danger px-3' onClick={() => handleDeletePost(post.id)}>x</button>
+                </div>
+                <h5 class="card-title">{post.title}</h5>
+                <div className='card-text'>{post.body}</div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
